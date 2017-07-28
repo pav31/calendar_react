f4fcbbce52f5a811cd321819f44749e395baf296
PavelG <vargan.pg@gmail.com>
2017-07-28 02:04:57 -0400

    Converted FE to react

---
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -0,0 +1,30 @@
+var AppointmentForm = React.createClass({
+    handleChange: function(e) {
+        var name = e.target.name;
+        obj = {};
+        obj[name] = e.target.value;
+        this.props.onUserInput(obj);
+    },
+
+    handleSubmit: function(e) {
+        e.preventDefault();
+        this.props.onFormSubmit();
+    },
+
+    render: function() {
+        return (
+            <div>
+                <h2>Make a new appointment</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <input name="title" placeholder="Appointment title"
+                           value={this.props.input_title}
+                           onChange={this.handleChange} />
+                    <input name="appt_time" placeholder="Date and time"
+                           value={this.props.input_appt_time}
+                           onChange={this.handleChange} />
+                    <input type="submit" value="Create" />
+                </form>
+            </div>
+        )
+    }
+});
