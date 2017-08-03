f12d9d8d6405962bba3866084b4e74177bfc18f3
PavelG <vargan.pg@gmail.com>
2017-08-03 00:15:59 -0400

    Replace react-rails gem with react_on_rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require react
-//= require react_ujs
-//= require moment
-//= require react-datetime.min
-//= require components
 //= require_tree .
