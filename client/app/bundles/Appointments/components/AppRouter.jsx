280cfe144fbbbd85ff092dc71bc85846d42a2ab7
PavelG <vargan.pg@gmail.com>
2017-09-11 00:41:54 -0400

    7.5 Editing a record Part1

--- client/app/bundles/Appointments/components/AppRouter.jsx
+++ client/app/bundles/Appointments/components/AppRouter.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Appointments from './Appointments';
 import Appointment from './Appointment';
+import AppointmentForm from './AppointmentForm';
 import { AppHeader } from './AppHeader';
 
 export default (props) => {
     return (
         <Router>
             <div>
-                <AppHeader />
+                <Route path="/" component={AppHeader} />
                 <Route exact path="/" render={routeProps => (
                     <Appointments {...routeProps} appointments={props.appointments} />
                 )} />
 
-                <Route path="/appointments/:id" component={Appointment} />
+                <Route exact path="/appointments/:id" component={Appointment} />
+                <Route path="/appointments/:id/edit" component={AppointmentForm} />
             </div>
 
         </Router>
